e1eace5a6cdfe905ffe61d2ba01ebcd7e44b8ebe
gothmate <gothmate@gmail.com>
2023-07-13 20:01:35 -0300

    feature:  - criado card dos produtos

--- src/app/components/Main/index.tsx
+++ src/app/components/Main/index.tsx
@@ -1,11 +1,24 @@
 import Image from 'next/image'
 import styles from '../../page.module.sass'
 import Link from 'next/link'
+import Card from '../Card/index'
+
 
 export default function Main() {
   return (
     <main className={styles.mainPage}>
-      <h1>Main Page</h1>
+      <header className={styles.cardsHeader}>
+        <h2>Melhores ofertas</h2>
+        <h5>Ordenar</h5>
+      </header>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
     </main>
   )
 }
