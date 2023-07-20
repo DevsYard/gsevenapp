4745a8bba461e98827912f4f3b0d6ac94c8cf0e5
gothmate <gothmate@gmail.com>
2023-07-20 18:40:55 -0300

    Feature:  - criada conexão com a API na página de signup

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -3,21 +3,84 @@
 import Image from 'next/image'
 import styles from '../page.module.sass'
 import Link from 'next/link'
-import { useState } from 'react'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export default function SignIn() {
+	const [mostraSenha, setMostraSenha] = useState<string>('password');
+	const [username, setUsername] = useState<string>('');
+	const [password, setPassword] = useState<string>('');
 
-	const [mostraSenha, setMostraSenha] = useState<string>('password')
-	
-	function handleLogin() {
-		
+	// const [csrfToken, setCsrfToken] = useState<string>('');
+
+	const requests = axios.create({
+		baseURL: 'http://localhost:3001',
+		timeout: 1000,
+	});
+
+	function handleLogin(e: any) {
+		e.preventDefault();
+
+		if (password.length < 8) {
+			alert('A Senha precisa ter pelo menos 8 dígitos');
+			return;
+		}
+
+		const data = {
+			username: username,
+			password: password,
+		};
+
+		requests
+			.get('/signin')
+			.then((res) => {
+				console.log(res);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}
+
+	function handleUsername(e: any) {
+		setUsername(e.target.value);
+	}
+	function handlePassword(e: any) {
+		setPassword(e.target.value);
 	}
 
 	function handleMostrarSenha(e: any) {
-		e.target.checked ? setMostraSenha('text') : setMostraSenha('password')
+		e.target.checked ? setMostraSenha('text') : setMostraSenha('password');
 	}
 
-  return (
+	useEffect(() => {
+		const requests = axios.create({
+			baseURL: 'https://localhost:3001',
+			timeout: 1000,
+		});
+
+		requests
+			.get('/signin')
+			.then((res) => {
+				console.log(res);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+
+		// async function fetchCsrfToken() {
+		// 	try {
+		// 		const response = await requests.get('/signin' || '/')
+		// 		const token = response.data.local.csrfToken
+		// 		setCsrfToken(token)
+		// 	} catch(err) {
+		// 		console.error("Error fetching csrf token", err)
+		// 	}
+		// }
+
+		// fetchCsrfToken()
+	}, []);
+
+	return (
 		<main className={styles.mainLogin}>
 			<aside className={styles.aside}>
 				<Image
@@ -30,15 +93,22 @@ export default function SignIn() {
 				/>
 			</aside>
 			<form action='submit' className={styles.rightScreen}>
+				{/* <input type='hidden' name='_csrf' value={csrfToken} /> */}
 				<input
 					placeholder='Nome de usuário'
 					className={styles.input}
 					type='text'
+					name='username'
+					onChange={handleUsername}
+					autoComplete={username}
 				/>
-				<input 
-					placeholder='Senha' 
-					className={styles.input} 
-					type={mostraSenha} 
+				<input
+					placeholder='Senha'
+					className={styles.input}
+					type={mostraSenha}
+					name='password'
+					onChange={handlePassword}
+					autoComplete={password}
 				/>
 				<label className={styles.checkLabel}>
 					Mostrar senha
@@ -48,7 +118,11 @@ export default function SignIn() {
 						onChange={(event) => handleMostrarSenha(event)}
 					/>
 				</label>
-				<button className={styles.inputBtn} onClick={() => handleLogin} type='button'>
+				<button
+					className={styles.inputBtn}
+					onClick={(e) => handleLogin(e)}
+					type='button'
+				>
 					Logar
 				</button>
 				<button className={styles.inputGoogle} type='button'>
@@ -78,5 +152,5 @@ export default function SignIn() {
 				</p>
 			</form>
 		</main>
-	)
+	);
 }
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -4,16 +4,76 @@ import Image from 'next/image'
 import styles from '../page.module.sass'
 import Link from 'next/link';
 import { HTMLInputTypeAttribute, useState } from 'react';
+import axios from 'axios';
 
 export default function SignIn() {
+	const [mostraSenha, setMostraSenha] =
+		useState<HTMLInputTypeAttribute>('password');
+	const [username, setUsername] = useState<string>('');
+	const [password, setPassword] = useState<string>('');
+	const [repeatPassword, setRepeatPassword] = useState<string>('');
+	const [admin, setAdmin] = useState<boolean>(false);
+	// const [csrfToken, setCsrfToken] = useState<string>('')
 
-	const [mostraSenha, setMostraSenha] = useState<HTMLInputTypeAttribute>('password')
+	const requests = axios.create({
+		baseURL: 'http://localhost:3001',
+		// timeout: 7000,
+	});
 
 	function handleMostrarSenha(e: any) {
 		e.target.checked ? setMostraSenha('text') : setMostraSenha('password');
 	}
 
-  return (
+	function handleAdmin(e: any) {
+		e.target.value === 'user'
+			? setAdmin(false)
+			: e.target.value === 'admin'
+			? setAdmin(true)
+			: alert('Escolha o tipo de conta');
+		console.log('É admin?', e.target.value);
+	}
+
+	function handleUsername(e: any) {
+		setUsername(e.target.value);
+	}
+	function handlePassword(e: any) {
+		setPassword(e.target.value);
+	}
+	function handleRepeatPassword(e: any) {
+		setRepeatPassword(e.target.value);
+	}
+
+	function handleSubmit(e: any) {
+		e.preventDefault();
+
+		if (password !== repeatPassword) {
+			alert('As senhas precisam ser iguais');
+			return;
+		}
+
+		if (password.length < 8) {
+			alert('As senhas precisam ter pelo menos 8 caracteres');
+			return;
+		}
+
+		const data = {
+			username: username,
+			password: password,
+			admin: admin,
+		};
+
+		requests
+			.post('/signup', data)
+			.then((res) => {
+				console.log('O envio: ', data);
+				console.log('essa é a resposta: ', res);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}
+
+	return (
 		<main className={styles.mainLogin}>
 			<aside className={styles.aside}>
 				<Image
@@ -26,20 +86,24 @@ export default function SignIn() {
 				/>
 			</aside>
 			<form action='submit' className={styles.rightScreen}>
+				{/* <input type='hidden' name='_csrf' value={csrfToken} /> */}
 				<input
 					placeholder='Nome de usuário'
 					className={styles.input}
 					type='text'
+					onChange={(e) => handleUsername(e)}
 				/>
 				<input
 					placeholder='Senha'
 					className={styles.input}
 					type={mostraSenha}
+					onChange={(e) => handlePassword(e)}
 				/>
 				<input
 					placeholder='Repetir senha'
 					className={styles.input}
 					type={mostraSenha}
+					onChange={(e) => handleRepeatPassword(e)}
 				/>
 				<label className={styles.checkLabel}>
 					Mostrar senha
@@ -57,16 +121,28 @@ export default function SignIn() {
 							className={styles.checkers}
 							name='userType'
 							type='radio'
+							value='user'
+							onChange={(e) => handleAdmin(e)}
 						/>
 						Usuário
 					</label>
 					<label className={styles.radio} htmlFor='userType'>
-						<input className={styles.checkers} name='userType' type='radio' />
+						<input
+							className={styles.checkers}
+							name='userType'
+							type='radio'
+							value='admin'
+							onChange={(e) => handleAdmin(e)}
+						/>
 						Admin
 					</label>
 				</div>
 
-				<button className={styles.inputBtn} type='button'>
+				<button
+					className={styles.inputBtn}
+					type='submit'
+					onClick={(e) => handleSubmit(e)}
+				>
 					Criar conta
 				</button>
 
